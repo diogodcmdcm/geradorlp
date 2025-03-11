bc337ff4750f35bc4c026030d0c29fca9a5c545d
diogodcmdcm <diogo.micheluzzi@gmail.com>
2025-03-11 01:09:20 -0300

    first commit

--- src/geradorlp_frontend/src/App.jsx
+++ src/geradorlp_frontend/src/App.jsx
@@ -1,31 +1,24 @@
-import { useState } from 'react';
-import { geradorlp_backend } from 'declarations/geradorlp_backend';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Index from './pages/Index';
+import Home from './pages/home';
+import T1Pageconfig from './pages/t1pageconfig';
+import Page from './pages/page';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    geradorlp_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+function App(){
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/t1pageconfig/:nome" element={<T1Pageconfig/>} />
+        <Route path="/page/:nome" element={<Page/>} />
+      </Routes>
+    </Router>
+
   );
+
 }
 
 export default App;
